d284334ec73d48857ed8f74edc0ad55677d2f903
Cynthia Kupski <cynthia.kupski@lmnsolutions.com>
2014-05-16 09:43:36 -0400

    all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cindybook::Application.config.secret_token = '1ae1f837dc03d7cf1b8b812a1affa430709de40d779ff3e4f2eb961da3efb42c58f0fb498621d911ab748a730c6ccb00697a3d3dd843d91fc89a4ea812ababb9'
